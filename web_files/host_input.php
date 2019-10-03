c6701724e0a441f440fc5bd87c5406417dd10823
Alden Hilton <alden711@gmail.com>
2019-10-03 01:16:35 -0600

    Deleted unused file.

--- web_files/host_input.php
+++
@@ -1,7 +0,0 @@
-<form>
-    <input>
-    <select>
-        <option value="address">IP Address (ie 192.0.2.1)</option>
-        <option value="prefix">Prefix (ie 192.0.2.1/24)</option>
-    </select>
-</form>
